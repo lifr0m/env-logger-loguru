f21948899cecaf3abc486952d9f6009be6dd3dd0
Lifr0m <artem.kay@yandex.ru>
2025-01-25 19:42:52 +0300

    fix logging without formatting arguments

--- src/lib.rs
+++ src/lib.rs
@@ -15,56 +15,56 @@ pub fn log_internal(level: Level, args: std::fmt::Arguments) {
 
 #[macro_export]
 macro_rules! log {
-    ($level:expr, $format:expr, $($arg:tt)*) => {
-        $crate::log_internal($level, format_args!($format, $($arg)*))
+    ($level:expr, $($arg:tt)+) => {
+        $crate::log_internal($level, format_args!($($arg)+))
     };
 }
 
 #[macro_export]
 macro_rules! critical {
-    ($format:expr, $($arg:tt)*) => {
-        $crate::log_internal($crate::Level::Critical, format_args!($format, $($arg)*))
+    ($($arg:tt)+) => {
+        $crate::log_internal($crate::Level::Critical, format_args!($($arg)+))
     };
 }
 
 #[macro_export]
 macro_rules! error {
-    ($format:expr, $($arg:tt)*) => {
-        $crate::log_internal($crate::Level::Error, format_args!($format, $($arg)*))
+    ($($arg:tt)+) => {
+        $crate::log_internal($crate::Level::Error, format_args!($($arg)+))
     };
 }
 
 #[macro_export]
 macro_rules! warning {
-    ($format:expr, $($arg:tt)*) => {
-        $crate::log_internal($crate::Level::Warning, format_args!($format, $($arg)*))
+    ($($arg:tt)+) => {
+        $crate::log_internal($crate::Level::Warning, format_args!($($arg)+))
     };
 }
 
 #[macro_export]
 macro_rules! success {
-    ($format:expr, $($arg:tt)*) => {
-        $crate::log_internal($crate::Level::Success, format_args!($format, $($arg)*))
+    ($($arg:tt)+) => {
+        $crate::log_internal($crate::Level::Success, format_args!($($arg)+))
     };
 }
 
 #[macro_export]
 macro_rules! info {
-    ($format:expr, $($arg:tt)*) => {
-        $crate::log_internal($crate::Level::Info, format_args!($format, $($arg)*))
+    ($($arg:tt)+) => {
+        $crate::log_internal($crate::Level::Info, format_args!($($arg)+))
     };
 }
 
 #[macro_export]
 macro_rules! debug {
-    ($format:expr, $($arg:tt)*) => {
-        $crate::log_internal($crate::Level::Debug, format_args!($format, $($arg)*))
+    ($($arg:tt)+) => {
+        $crate::log_internal($crate::Level::Debug, format_args!($($arg)+))
     };
 }
 
 #[macro_export]
 macro_rules! trace {
-    ($format:expr, $($arg:tt)*) => {
-        $crate::log_internal($crate::Level::Trace, format_args!($format, $($arg)*))
+    ($($arg:tt)+) => {
+        $crate::log_internal($crate::Level::Trace, format_args!($($arg)+))
     };
 }
